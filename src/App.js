5a1a46672ad6f3f8260516420709bc8952eaf46e
IviKeiko <jooly.klee@gmail.com>
2023-02-26 17:07:37 +0100

    css changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-// import { useFetchShows } from "./components/useFetchShows";
-
 import Header from "./components/Header";
 import Shows from "./components/Shows";
 import Footer from "./components/Footer";
 import Pagination from "./components/pagination";
 
 function App() {
+  const [defaultShows, setDefaultShows] = useState([]);
   const [shows, setShows] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
@@ -23,6 +22,7 @@ function App() {
       setLoading(true);
       const response = await fetch("http://api.tvmaze.com/shows");
       const showsData = await response.json();
+      setDefaultShows(showsData);
       setShows(showsData);
       setLoading(false);
     };
@@ -39,7 +39,7 @@ function App() {
 
   //SEARCH
   useEffect(() => {
-    const filteredShows = shows.filter((show) => {
+    const filteredShows = defaultShows.filter((show) => {
       return show.name.toLowerCase().includes(search);
     });
     setShows(filteredShows); // ??
@@ -47,6 +47,7 @@ function App() {
 
   const searchHandler = (e) => {
     const searchValue = e.target.value;
+
     setSearch(searchValue);
   };
 
@@ -65,53 +66,3 @@ function App() {
   );
 }
 export default App;
-
-// function App() {
-//   const { loading, showsData } = useFetchShows();
-
-//   const [shows, setShows] = useState([]);
-
-//   const [page, setPage] = useState(0);
-
-//   const [filteredShows, setFilteredShows] = useState([]);
-
-//   const [search, setSearch] = useState("");
-
-//
-
-//   const nextPage = () => {
-//     setPage((currentPage) => {
-//       let nextPage = currentPage + 1;
-//       if (nextPage > showsData.length - 1) {
-//         nextPage = 0;
-//       }
-//       return nextPage;
-//     });
-//   };
-
-//   const prevPage = () => {
-//     setPage((currentPage) => {
-//       let prevPage = currentPage - 1;
-//       if (prevPage < 0) {
-//         prevPage = showsData.length - 1;
-//       }
-//       return prevPage;
-//     });
-//   };
-
-//   return (
-//     <>
-//       <Header onChange={searchHandler} />
-//       <Shows
-//         showList={shows}
-//         filteredShows={filteredShows}
-//         showsData={showsData}
-//         page={page}
-//         onClick={handlePage}
-//         nextPage={nextPage}
-//         prevPage={prevPage}
-//       />
-//       <Footer />
-//     </>
-//   );
-// }
